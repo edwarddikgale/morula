ca3f8a39f7c6b5b6f71dfd00cb4fc368f71c93a5
Edward Dikgale <edwarddikgale@Mac.cds.testo>
2025-05-16 14:22:15 +0200

    upgraded YAML's for CI/CD based on firebase & github actions deployment CLI

--- src/dashboard/components/Title.tsx
+++ src/dashboard/components/Title.tsx
@@ -9,7 +9,7 @@ const Title = () => {
 
       <GreetUser prefixMessage="Welcome back" />
       <p className='mt-3 text-muted'> 
-        Get the most of this app by filling in your profile details, using my events to create a unique list of best possible actions and adding ISO 20121 evidence to meet reporting requirements.
+        Welcome to Morula v1.0
       </p>
     </div>
   );
